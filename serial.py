cb3facf93e4abad89117b82d7c808cddfc39b91e
Eric Moore <4chrise8@gmail.com>
2024-03-20 09:55:31 -0700

    Finished serial.py

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        "Initializes the generator with a starting int value."
+        self.start = start
+        self.value = start
+
+    def generate(self) -> int:
+        "Increments the counter and returns the pre-incremented value, giving a 'generated' serial value."
+        self.value += 1
+        return self.value - 1
+    
+    def reset(self) -> None:
+        "Resets the counter value to the start value."
+        self.value = self.start
+    
+    def __repr__(self) -> str:
+        return f"SerialGenerator(start={self.start}, value={self.value})"
